d4cd2016996b9d6167421a054cb2a357ee9638ef
Ruba <s12028675@stu.najah.edu>
2025-03-25 03:10:09 +0200

    Add Procedure for Add new Borrower

---
+++ Functions/MostPopularGenre.sql
@@ -0,0 +1,25 @@
+CREATE FUNCTION fn_PopularGenrePerMonth(@month INT)
+RETURNS TABLE
+AS 
+RETURN
+(
+	WITH PopularBooks AS
+	(
+		SELECT BookID,
+		DENSE_RANK() OVER(ORDER BY COUNT(*)  DESC) as Rank
+
+		FROM Loans
+		WHERE Month(DateBorrowed) = @month
+		GROUP BY BookID
+	),
+	MostPopularBook AS
+	(
+		SELECT TOP 1 * FROM PopularBooks
+	)
+	select  g.*
+	from Books b
+	JOIN Genres g
+	ON b.GenreID = g.GenreID
+	JOIN MostPopularBook m
+	ON m.BookID = b.BookID
+)
