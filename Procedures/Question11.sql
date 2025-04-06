7e3df310eeee6213a8659b43ced60e77c7420b48
Ruba <s12028675@stu.najah.edu>
2025-04-06 17:39:28 +0300

    Update Question 11

---
+++ Procedures/Question11.sql
@@ -0,0 +1,16 @@
+DROP PROCEDURE IF EXISTS sp_BorrowedBooksReport
+GO
+
+CREATE PROCEDURE sp_BorrowedBooksReport (@startDate DATE, @endDate DATE)
+AS
+BEGIN
+	SELECT b.BookID, b.Title, bor.BorrowerID, CONCAT(bor.FirstName,' ', bor.LastName) as borrowerName,
+	l.DateBorrowed
+	FROM Loans l
+	JOIN Books b
+	ON l.BookID = b.BookID
+	JOIN Borrowers bor
+	ON l.BorrowerID = bor.BorrowerID
+	WHERE
+		l.DateBorrowed >= @startDate AND l.DateBorrowed <= @endDate
+END
